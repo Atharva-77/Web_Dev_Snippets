d0ebe44fd942945124b685905ec5f33fa4ca7395
Atharva2018 <atharva.shirode99@gmail.com>
2021-01-06 22:29:06 +0530

    Sidebar updated with passing onclick between diff components

--- responsive_left_sidebar/src/Navbar.js
+++ responsive_left_sidebar/src/Navbar.js
@@ -1,15 +1,9 @@
 import React, { useState } from 'react';
-// import * as FaIcons from 'react-icons/fa';
-// import * as AiIcons from 'react-icons/ai';
-// import { Link } from 'react-router-dom';
 
-import MenuIcon from '@material-ui/icons/Menu'
-import CloseIcon from '@material-ui/icons/Close';
-
-import { Sidebar } from './Sidebar';
+import Header from './Header.js'
+import Nav2 from './Nav2.js'
 
 import './Navbar.css';
-// import { IconContext } from 'react-icons';
 
 function Navbar() {
   const [sidebar, setSidebar] = useState(false);
@@ -18,34 +12,9 @@ function Navbar() {
 
   return (
     <div>
-      {/* <IconContext.Provider value={{ color: '#fff' }}> */}
-        <div className='navbar'>
-          {/* <Link to='#' className='menu-bars'> */}
-            <MenuIcon onClick={showSidebar} />
-            <h1>Heading</h1>
-            <h4>Yoo</h4>
-          {/* </Link> */}
-        </div>
-        <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
-          <ul >
+      <Header sidebar={sidebar} setSidebar={setSidebar}/>
 
-                                                                         {/* <li className='navbar-toggle'> */}
-                {/* <MenuIcon onClick={showSidebar} />                                                    <Link to='#' className='menu-bars'> */}
-                <MenuIcon onClick={showSidebar} className="nav-menu-left"/>
-                                                                            {/* </Link> */}{/* </li> */}
-            {Sidebar.map((item, index) => {
-              return (
-                <li key={index} className={item.cName}>
-                                                                                    {/* <Link to={item.path}> */}
-                    {item.icon}
-                    <span>{item.title}</span>
-                                                                                      {/* </Link> */}
-                </li>
-              );
-            })}
-          </ul>
-        </nav>
-      {/* </IconContext.Provider> */}
+        <Nav2 sidebar={sidebar} setSidebar={setSidebar}/>
     </div>
   );
 }
