6ac7adb50af2157b12ce84540fa856c464d9ef9f
Atharva2018 <atharva.shirode99@gmail.com>
2021-02-13 19:59:13 +0530

    Increment by 5, fun vs obj

--- React_Basics/Class/src/SetState.js
+++ React_Basics/Class/src/SetState.js
@@ -11,9 +11,41 @@ export class SetState extends Component {
     }
     increment()
     {
-        this.state.count = this.state.count + 1
-        console.log(this.state.count)
+		//this.state.count = this.state.count + 1
+-        	//console.log(this.state.count)
+
+        // this.setState(
+        //     {
+        //         count: this.state.count+ 1
+        //     },
+        //     () =>
+        //     {
+        //         console.log("Callback value",this.state.count)
+        //     }
+        // ) 
+        // console.log(this.state.count) //lag by 1. Therefore put all code after setState in callback
+
+        //If obj is passed, opof incrementFive is 1 as all 5 statements are combined & 1 call made.Thus pass function NOT obj.
+
+        this.setState((prevCnt)=>(
+        {
+            count:prevCnt.count + 1
+        }),
+        ()=>
+        {
+          console.log("Callback 5 value",this.state.count)
+        }
+        )
      
+    }
+
+    incrementFive()
+    {
+        this.increment()
+        this.increment()
+        this.increment()
+        this.increment()
+        this.increment()
     }
     
     render() {
@@ -21,6 +53,8 @@ export class SetState extends Component {
             <div>
                 <div>Count- {this.state.count}</div>
                 <button onClick={()=> this.increment()}>Increment</button>
+                <button onClick={()=> this.incrementFive()}>Increment5</button>
+
             </div>
         )
     }
